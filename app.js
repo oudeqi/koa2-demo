40a33e08a3c95da53259fc0959b0d5fe40321069
oudeqi <496997374@qq.com>
2018-11-26 09:07:15 +0800

    access_tkoen

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const Koa = require('koa')
-const sha1 = require('sha1')
 const app = new Koa()
 const views = require('koa-views')
 const json = require('koa-json')
@@ -8,25 +7,12 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 
 const config = require('./lib/config')
+const wechat = require('./lib/middleware')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
 // error handler
 onerror(app)
-
-// 微信认证
-app.use(async (ctx, next) => {
-  console.log(ctx.query)
-  const {signature, timestamp, nonce, echostr} = ctx.query
-  const token = config.wechat.token
-  let str = [token, timestamp, nonce].sort().join('')
-  const sha = sha1(str)
-  if (sha === signature) {
-    ctx.body = echostr
-  } else {
-    ctx.body = 'wrong'
-  }
-})
 
 // middlewares
 app.use(bodyparser({
@@ -35,6 +21,9 @@ app.use(bodyparser({
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
+
+// 微信认证
+app.use(wechat(config.wechat))
 
 app.use(views(__dirname + '/views', {
   extension: 'pug'
